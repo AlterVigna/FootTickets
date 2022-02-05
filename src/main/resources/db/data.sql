02c11264a3845f2d7145eda34b57ef116388e0c1
Davide <davide.batista@hotmail.it>
2022-02-05 12:31:08 +0100

    Last review: added new default user for tests and change max timeout

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -1,5 +1,7 @@
 INSERT IGNORE INTO footticket.account VALUES ("user","pass",500,"B");
 INSERT IGNORE INTO footticket.account VALUES ("admin","pass",500,"A");
+INSERT IGNORE INTO footticket.account VALUES ("alice","pass",1000,"B");
+INSERT IGNORE INTO footticket.account VALUES ("bob","pass",1000,"B");
 
 
 INSERT IGNORE INTO footticket.transaction (`id`,`numSeats`,`location`, `price`,`username`) VALUES (1,3,'A1-A2-A3','150.00','user');
